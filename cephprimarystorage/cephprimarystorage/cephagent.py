fc7bc0a0d55131bc853e98da237ee416402780b4
majin <630911437@qq.com>
2017-04-06 14:31:07 +0800

    Revert "add chinese support when add ceph pool"
    This reverts commit 4527f93b30030a336276ee8374d616e4281ac89a.

--- cephprimarystorage/cephprimarystorage/cephagent.py
+++ cephprimarystorage/cephprimarystorage/cephagent.py
@@ -441,12 +441,11 @@ class CephAgent(object):
 
         pool_names = existing_pools.split("\n")
 
-        realname = eval('u"' + cmd.poolName + '"').encode('utf-8')
-        if cmd.errorIfNotExist and realname not in pool_names:
-            raise Exception('cannot find the pool[%s] in the ceph cluster, you must create it manually' % realname)
+        if cmd.errorIfNotExist and cmd.poolName not in pool_names:
+            raise Exception('cannot find the pool[%s] in the ceph cluster, you must create it manually' % cmd.poolName)
 
-        if realname not in pool_names:
-            shell.call('ceph osd pool create %s 100' % realname)
+        if cmd.poolName not in pool_names:
+            shell.call('ceph osd pool create %s 100' % cmd.poolName)
 
         return jsonobject.dumps(AgentResponse())
 
